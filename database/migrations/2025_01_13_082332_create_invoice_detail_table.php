6c70ed24bb2978284623d489edc2198c20c58b88
Ryan S <ryan.nash29@gmail.com>
2025-01-13 15:30:14 +0700

    yanto

---
+++ database/migrations/2025_01_13_082332_create_invoice_detail_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_detail', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_id');
+            $table->string('deskripsi');
+            $table->integer('harga');
+            $table->integer('qty');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_detail');
+    }
+};
